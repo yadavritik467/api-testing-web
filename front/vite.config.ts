b269821de6756ebc27266f19539a20c2e9691e83
Ritik Yaduvanshi <yadavritik467@gmail.com>
2025-12-17 19:56:11 +0530

    updated repo url to deploy front

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build:{
-    outDir:"public_html"
+  base: "/api-testing-web/front/",
+  build: {
+    outDir: "dist",
   },
   plugins: [react()],
   server: {
