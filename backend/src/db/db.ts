7c46945d49bdb109fefb87befc5ef79afb0334a2
Ritik Yaduvanshi <yadavritik467@gmail.com>
2025-12-17 19:44:56 +0530

    created backend and dockerize forntend and backend

---
+++ backend/src/db/db.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+export const dbConnection = async () => {
+  let isConnected = false;
+  try {
+    console.log("please wait db is tring to connecting ..");
+    await mongoose.connect("mongodb://mongo:27017/api-testing-web");
+    console.log("db connected");
+  } catch (error: any) {
+    console.log("db connection failed", error.message);
+  }
+};
