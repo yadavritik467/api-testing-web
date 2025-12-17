191359f650268a282fd2d3609c28817191bf308d
Ritik Yaduvanshi <yadavritik467@gmail.com>
2025-12-17 21:18:51 +0530

    change base name

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/api-testing-web/",
   build: {
     outDir: "dist",
   },
