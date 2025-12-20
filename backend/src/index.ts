5da0b068c05afac9772547e5bcc94b68cfcc7acb
Ritik Yaduvanshi <yadavritik467@gmail.com>
2025-12-20 17:19:55 +0530

    test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,12 +11,16 @@ app.use(
   })
 );
 
+
+
 dbConnection()
 
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("hii from server and its working good !!");
+
+
+  res.send("hii from server and its working  !!");
 });
  
 app.listen(port, () => {
