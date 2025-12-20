f88e3041cce557475fa36c88b7e6d646d6a83790
yadavritik467 <yadavritik467>
2025-12-20 11:31:47 +0000

    chore(backend): apply prettier formatting

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,11 +1,11 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 export const dbConnection = async () => {
-  let isConnected = false;
+  let isConnected = false
   try {
-    console.log("please wait db is tring to connecting ..");
-    await mongoose.connect("mongodb://mongo:27017/api-testing-web");
-    console.log("db connected");
+    console.log('please wait db is tring to connecting ..')
+    await mongoose.connect('mongodb://mongo:27017/api-testing-web')
+    console.log('db connected')
   } catch (error: any) {
-    console.log("db connection failed", error.message);
+    console.log('db connection failed', error.message)
   }
-};
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,41 +1,25 @@
-import cors from "cors";
-import express, { type Request, type Response } from "express";
-import { dbConnection } from "./db/db.js";
+import cors from 'cors'
+import express, { type Request, type Response } from 'express'
+import { dbConnection } from './db/db.js'
 
-const app = express();
+const app = express()
 
-
-
-
-
-
-
-
-const port = 4500;
-
-
-
-
+const port = 4500
 
 app.use(
   cors({
-    origin: "http://localhost:5174",
+    origin: 'http://localhost:5174',
   })
-);
+)
 
 dbConnection()
 
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('hii from server and its working good !!')
+})
 
-
-
-
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("hii from server and its working good !!");
-});
- 
 app.listen(port, () => {
-  console.log(`server is running on  ${port}`);
-});
+  console.log(`server is running on  ${port}`)
+})
