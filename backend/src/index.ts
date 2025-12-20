6a1e14c1ce181312deb3e7aae2560b694219429b
Ritik Yaduvanshi <130385697+yadavritik467@users.noreply.github.com>
2025-12-20 17:45:42 +0530

    test (#8)
    * test
    
    * chore(backend): apply prettier formatting
    
    * upadated command
    
    ---------
    
    Co-authored-by: yadavritik467 <yadavritik467>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,24 @@
-import cors from "cors";
-import express, { type Request, type Response } from "express";
-import { dbConnection } from "./db/db.js";
+import cors from 'cors'
+import express, { type Request, type Response } from 'express'
+import { dbConnection } from './db/db.js'
 
-const app = express();
+const app = express()
 
-const port = 4500;
+const port = 4500
 app.use(
   cors({
-    origin: "http://localhost:5174",
+    origin: 'http://localhost:5174',
   })
-);
+)
 
 dbConnection()
 
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("hii from server and its working good !!");
-});
- 
+app.get('/', (req: Request, res: Response) => {
+  res.send('hii from server and its working  !!')
+})
+
 app.listen(port, () => {
-  console.log(`server is running on  ${port}`);
-});
+  console.log(`server is running on  ${port}`)
+})
