2f6a5928f3db7025da924d2dcebbd8bd699d9320
Ritik Yaduvanshi <yadavritik467@gmail.com>
2025-12-21 16:44:27 +0530

    added eslint in backend

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose'
 export const dbConnection = async () => {
-  let isConnected = false
-
   try {
-    console.log('please wait db is tring to connecting ..')
-
     await mongoose.connect('mongodb://mongo:27017/api-testing-web')
-
-    console.log('db connected')
-  } catch (error: any) {
-    console.log('db connection failed', error.message)
+    // console.log('db connected')
+  } catch (error) {
+    if(error instanceof Error){
+      return error?.message
+    }
+    return String(error)
+    // console.log('db connection failed', error.message)
   }
 }
