15850419ecbd63ab8900c98bd6ef92139c83e4b3
Ritik Yaduvanshi <yadavritik467@gmail.com>
2025-12-21 16:47:52 +0530

    formatted backend  code

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
-=======
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
->>>>>>> 2f6a592 (added eslint in backend)
 
 export default tseslint.config(
   js.configs.recommended,
@@ -12,16 +7,12 @@ export default tseslint.config(
   {
     languageOptions: {
       parserOptions: {
-<<<<<<< HEAD
         project: './tsconfig.json',
-=======
-        project: "./tsconfig.json", 
->>>>>>> 2f6a592 (added eslint in backend)
+        project: './tsconfig.json',
         tsconfigRootDir: import.meta.dirname,
       },
     },
     rules: {
-<<<<<<< HEAD
       '@typescript-eslint/no-unused-vars': [
         'error',
         { argsIgnorePattern: '^_' },
@@ -34,14 +25,3 @@ export default tseslint.config(
     ignores: ['dist/', 'node_modules/', 'eslint.config.mjs'],
   }
 )
-=======
-      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-      "@typescript-eslint/no-explicit-any": "error", 
-      "no-console": "warn",
-    },
-  },
-  {
-    ignores: ["dist/", "node_modules/","eslint.config.mjs"], 
-  }
-);
->>>>>>> 2f6a592 (added eslint in backend)
