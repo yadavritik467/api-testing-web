b3a3f36e66bee9a4bb58f58fd8397d2bab027030
Ritik Yaduvanshi <yadavritik467@gmail.com>
2025-12-25 22:31:33 +0530

    small change

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,6 +20,6 @@ app.get('/', (req: Request, res: Response) => {
   res.send('hii from EC2 !!')
 })
 
-app.listen(port,'0.0.0.0', () => {
+app.listen(port, () => {
   logger.info(`server is running on  ${port}`)
 })
