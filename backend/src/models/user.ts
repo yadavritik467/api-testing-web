08e97fa6249d8ebcb6caa92c8ac9598dae725d60
ritik yaduvanshi <yadavritik467@gmail.com>
2026-01-16 13:58:15 +0530

    created user register api and added password hashing and token generating

---
+++ backend/src/models/user.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+)
+
+export const User = mongoose.model('User', userSchema)
---
+++ backend/src/utils/security.ts
@@ -0,0 +1,21 @@
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET } from '../config/environment.js'
+
+export const hashed_password = async (password: string): Promise<string> => {
+  const hashed = await bcrypt.hash(password, 10)
+  return hashed
+}
+
+export const compare_password = async (
+  password: string,
+  hash: string
+): Promise<boolean> => {
+  const is_matched = await bcrypt.compare(password, hash)
+  return is_matched
+}
+
+export const generate_token = (userId: string): string => {
+  const token = jwt.sign({ _id: userId }, JWT_SECRET)
+  return token
+}
